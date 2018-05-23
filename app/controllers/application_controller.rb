79e172c3fa1f36e865ede7d7fb6bf80f1a26a490
Jean Pierre Coymat <jpcoymat@gmail.com>
2018-05-23 16:17:15 +0000

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  layout :define_layout
+
+
+  private
+
+  def define_layout
+    devise_controller? ? "devise" : "application"
+  end
+
+
 end
